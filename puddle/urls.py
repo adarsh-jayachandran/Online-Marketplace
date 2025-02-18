f6801779cb7ae32721435cb93dcc39243c3260b4
e <adarshpnr13@gmail.com>
2025-02-18 20:54:02 +0530

    completed the detals app

--- puddle/urls.py
+++ puddle/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static # for image
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/',contact, name = 'contact'),
-    path('', index,name='index')
-] 
+    path('', index,name='index'),
+    path('items/', include('items.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # for images
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # for images
